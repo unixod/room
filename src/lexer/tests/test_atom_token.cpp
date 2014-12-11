32b9eccafb222c0008842b1727d4143b7824d6d8
Eldar Zakirov <unixod@gmail.com>
2014-12-11 22:57:08 +0300

    code clean

--- src/lexer/tests/test_atom_token.cpp
+++ src/lexer/tests/test_atom_token.cpp
@@ -11,20 +11,7 @@ namespace core = room::core;
 namespace utils = room::utils;
 namespace token = room::core::token;
 
-/// Helpers
-std::string unescape(std::string lexeme) {
-    if ((lexeme.size() > 2) &&          // is multi-escaped
-            (lexeme.front() == '|') &&
-            (lexeme.back() == '|')) {
-        lexeme.erase(lexeme.size() - 1).erase(0, 1);
-    } else {
-        auto trash = std::remove(lexeme.begin(), lexeme.end(), '\\');
-        lexeme.erase(trash, lexeme.end());
-    }
-
-    return lexeme;
-}
-
+/// Catch helpers
 namespace std {
 std::ostream & operator << (std::ostream &out, const core::Token &token) {
 
@@ -41,7 +28,44 @@ std::ostream & operator << (std::ostream &out, const core::Token &token) {
 }
 }
 
+/// Helpers
+std::string unescape(std::string lexeme) {
+    if ((lexeme.size() > 2) &&          // is multi-escaped
+            (lexeme.front() == '|') &&
+            (lexeme.back() == '|')) {
+        lexeme.erase(lexeme.size() - 1).erase(0, 1);
+    } else {
+        auto trash = std::remove(lexeme.begin(), lexeme.end(), '\\');
+        lexeme.erase(trash, lexeme.end());
+    }
+
+    return lexeme;
+}
+
 typedef std::vector<core::Token> Tokens;
+
+namespace tok {
+template<class... T>
+Tokens set(T&&... tokens) {
+    return {std::forward<T>(tokens)...};
+}
+
+core::Token atom(const std::string &lexeme) {
+    return {token::Class::Atom, lexeme};
+}
+
+core::Token end() {
+    return {token::Class::End, ""};
+}
+
+core::Token spaceBegin() {
+    return {token::Class::SpaceBegin, ""};
+}
+
+core::Token spaceEnd() {
+    return {token::Class::SpaceEnd, ""};
+}
+}
 
 Tokens tokenize(std::string pgm) {
     auto lexer = room::Lexer{
@@ -59,39 +83,25 @@ Tokens tokenize(std::string pgm) {
     return out;
 }
 
-template<class... T>
-Tokens tokens(T&&... tokens) {
-    return {std::forward<T>(tokens)...};
-}
-
 TEST_CASE("Atoms tokenization") {
     SECTION("in singular space") {
         SECTION("without escaping") {
-            REQUIRE(tokenize("a abc") == tokens(core::Token{token::Class::Atom, "a"},
-                                                core::Token{token::Class::Atom, "abc"},
-                                                core::Token{token::Class::End, ""}));
-
-            REQUIRE(tokenize("1 12 32") == tokens(core::Token{token::Class::Atom, "1"},
-                                                  core::Token{token::Class::Atom, "12"},
-                                                  core::Token{token::Class::Atom, "32"},
-                                                  core::Token{token::Class::End, ""}));
-
-            REQUIRE(tokenize("1a a2b") == tokens(core::Token{token::Class::Atom, "1a"},
-                                                 core::Token{token::Class::Atom, "a2b"},
-                                                 core::Token{token::Class::End, ""}));
-
-            REQUIRE(tokenize(". ...") == tokens(core::Token{token::Class::Atom, "."},
-                                                core::Token{token::Class::Atom, "..."},
-                                                core::Token{token::Class::End, ""}));
+            REQUIRE(tokenize("a abc") == tok::set(tok::atom("a"), tok::atom("abc"), tok::end()));
+            REQUIRE(tokenize("1 12 32") == tok::set(tok::atom("1"), tok::atom("12"), tok::atom("32"), tok::end()));
+            REQUIRE(tokenize("1a a2b") == tok::set(tok::atom("1a"), tok::atom("a2b"), tok::end()));
+            REQUIRE(tokenize(". ...") == tok::set(tok::atom("."), tok::atom("..."), tok::end()));
         }
     }
 
     SECTION("in singular space") {
-        REQUIRE(tokenize("(abc)") == tokens(core::Token{token::Class::SpaceBegin, ""},
-                                            core::Token{token::Class::Atom, "a"},
-                                            core::Token{token::Class::Atom, "b"},
-                                            core::Token{token::Class::Atom, "c"},
-                                            core::Token{token::Class::SpaceEnd, ""},
-                                            core::Token{token::Class::End, ""}));
+        REQUIRE(tokenize("(abc)") == tok::set(tok::spaceBegin(),
+                                              tok::atom("a"), tok::atom("b"), tok::atom("c"),
+                                              tok::spaceEnd(),
+                                              tok::end()));
+
+        REQUIRE(tokenize("(a abc)") == tok::set(tok::spaceBegin(),
+                                                tok::atom("a"), tok::atom(" "), tok::atom("a"), tok::atom("b"), tok::atom("c"),
+                                                tok::spaceEnd(),
+                                                tok::end()));
     }
 }
