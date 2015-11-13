ba3a2d10a35f2da0138e63d50cb4612589c839f5
Eldar Zakirov <unixod@gmail.com>
2015-11-13 22:48:27 +0300

    WIM, Parser: skeleton implemented

--- src/parser/src/parser.cpp
+++ src/parser/src/parser.cpp
@@ -1,11 +1,31 @@
 #include "room/parser.h"
 #include "room/parser/ast.h"
 #include "detail/alpha-parser.h"
+#include "detail/atom-tree.h"
 
 using room::parser::Ast;
 namespace detail = room::parser::detail;
 
+
+namespace {
+
+Ast makeAst(detail::AtomTree);
+
+} // namespace
+
+
 Ast room::parse(std::function<lexer::Token()> generator)
 {
+    auto atomTree = detail::alpha_parse(std::move(generator));
 
+    return makeAst(std::move(atomTree));
 }
+
+
+namespace {
+
+Ast makeAst(detail::AtomTree atomTree)
+{
+}
+
+} // namespace
