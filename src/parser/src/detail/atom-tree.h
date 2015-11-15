37d139caca80f39daaa8bf654d40c2b460896a2b
Eldar Zakirov <unixod@gmail.com>
2015-11-15 23:47:42 +0300

    WIP, AtomTree base implementation

--- src/parser/src/detail/atom-tree.h
+++ src/parser/src/detail/atom-tree.h
@@ -5,7 +5,28 @@ namespace room {
 namespace parser {
 namespace detail {
 
-class AtomTree {};
+class AtomTree {
+public:
+    struct Node;
+
+};
+
+struct AtomDescription;
+
+struct AtomTree::Node {
+    enum class Type {
+        Atom, Set
+    };
+
+    Type type;
+    Node* sibling = nullptr;
+    bool quoted = false;
+
+    union {
+        Node *child;
+        AtomDescription *descr;
+    };
+};
 
 } // namespace detail
 } // namespace parser
